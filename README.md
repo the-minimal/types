0f93eb82481f0ea8e1b2cc79eae79e4856bc29ca
Yamiteru <yamiteru@icloud.com>
2024-04-16 00:55:55 +0200

    feat: add basic types

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# types
+![Types image](https://github.com/the-minimal/types/blob/main/docs/the-minimal-types.jpg?raw=true)
+
+# @the-minimal/types
+
+Minimalist TypeScript type utility library with great focus on simplicity and performance.
+
+## Install
+
+```bash
+yarn add @the-minimal/types
+```
---
+++ index.d.ts
@@ -0,0 +1,17 @@
+export type Nullable<$Type> = $Type | null;
+
+export type Optional<$Type> = $Type | undefined;
+
+export type Nullish<$Type> = $Type | null | undefined;
+
+export type Either<$Left, $Right> = $Left | $Right;
+
+export type Predicate<$Type> = (value: unknown) => value is $Type;
+
+export type InferPredicate<$Predicate extends Predicate<unknown>> =
+	$Predicate extends Predicate<infer $Type> ? $Type : never;
+
+export type Assertion<$Type> = (value: unknown) => asserts value is $Type;
+
+export type InferAssertion<$Assertion extends Assertion<unknown>> =
+	$Assertion extends Assertion<infer $Type> ? $Type : never;

440eb35cf4d0658d6e713233a8d29780ef6d7a8f
Yamiteru <yamiteru@icloud.com>
2024-07-06 19:41:57 +0200

    feat: remove library-specific types

--- index.d.ts
+++ index.d.ts
@@ -2,18 +2,11 @@ export type Pretty<$Type> = { [$Key in keyof $Type]: $Type[$Key] } & {};
 
 export type Nullable<$Type> = $Type | null;
 
-export type Maybe<$Type> = $Type | undefined;
+export type Optional<$Type> = $Type | undefined;
 
 export type Nullish<$Type> = $Type | null | undefined;
 
 export type Either<$Left, $Right> = $Left | $Right;
-
-export type Assertion<$Type> = (value: unknown) => asserts value is $Type;
-
-export type UnknownAssertion = Assertion<unknown>;
-
-export type InferAssertion<$Assertion extends Assertion<unknown>> =
-	$Assertion extends Assertion<infer $Type> ? $Type : never;
 
 export type Intersection<$Values extends unknown[]> = $Values extends [
 	infer $Head,
